1815bf2b4aea2f104845c8df4b61a207b7ce69c4
Josh Campion <joshcampion89@gmail.com>
2016-04-12 09:17:38 +0100

    Initial Commit

---
+++ SelectionViewController/SelectionViewController.h
@@ -0,0 +1,19 @@
+//
+//  SelectionViewController.h
+//  SelectionViewController
+//
+//  Created by Josh Campion on 12/04/2016.
+//  Copyright © 2016 The Distance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelectionViewController.
+FOUNDATION_EXPORT double SelectionViewControllerVersionNumber;
+
+//! Project version string for SelectionViewController.
+FOUNDATION_EXPORT const unsigned char SelectionViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelectionViewController/PublicHeader.h>
+
+
