74ad9c623dcb0d2cd79a8baa99d4a3d1be6cdc66
Josh Campion <joshcampion89@gmail.com>
2016-04-12 11:39:19 +0100

    Added initial code + extra documentation

--- SelectionViewController/SelectionViewController.h
+++ SelectionViewController/SelectionViewController.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char SelectionViewControllerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SelectionViewController/PublicHeader.h>
 
 
+#import <SelectionViewController/TDSelectionViewController.h>
